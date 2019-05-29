65d6773209d461df964782440f69edd29021187d
nguyenkieungan <nguyen.kieu.ngan@framgia.com>
2019-05-29 14:38:27 +0700

    Updates

--- precache-manifest.4546c8bbc219ca29621db693b6a19984.js
+++ precache-manifest.4546c8bbc219ca29621db693b6a19984.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "078c73ccb1463a5bce7e",
+    "revision": "6ee4637880877373c987",
     "url": "/reactjs/static/css/main.9c2762a7.chunk.css"
   },
   {
-    "revision": "078c73ccb1463a5bce7e",
-    "url": "/reactjs/static/js/main.078c73cc.chunk.js"
+    "revision": "6ee4637880877373c987",
+    "url": "/reactjs/static/js/main.6ee46378.chunk.js"
   },
   {
     "revision": "4afef29974c8fff71c2a",
     "url": "/reactjs/static/js/runtime~main.4afef299.js"
   },
   {
-    "revision": "6c168958e1bf9273b8f5",
+    "revision": "c2a48331bc5dcd82d67d",
     "url": "/reactjs/static/css/2.7ccfc114.chunk.css"
   },
   {
-    "revision": "6c168958e1bf9273b8f5",
-    "url": "/reactjs/static/js/2.6c168958.chunk.js"
+    "revision": "c2a48331bc5dcd82d67d",
+    "url": "/reactjs/static/js/2.c2a48331.chunk.js"
   },
   {
-    "revision": "c0c97ece2859691196958e0e2d70899d",
+    "revision": "97d8535de8fedcbc453d124f937f0923",
     "url": "/reactjs/index.html"
   }
 ];
